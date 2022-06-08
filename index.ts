edef3b5c6f550a23dd4512308f2a53f7ae280af0
usuario@puerta18.corp.ar <usuario@puerta18.corp.ar>
2022-06-08 14:06:46 -0300

    actualizacion de Server

--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
-const nombre= "Irina Almazan";
-console.log(`Mi nombre es ${nombre}`)
+import {Servidor} from "./classes/server";
+import { router } from "./routes/router";
+import cors from "cors";
+
+let server = new Servidor();
+
+// Credenciales de seguridad
+server.app.use(cors({origin:true, credentials:true}));
+
+server.app.use('/', router)
+
+server.start( () => {
+    console.log("servidor corriendo en " + server.port);
+    
+});
